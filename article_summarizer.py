2a8391d190e9bdf45011017c039fc0eb1d67db51
Tushit Dev Tripathi <85063651+DevTushit17@users.noreply.github.com>
2024-03-30 15:56:02 +0530

    Update article_summarizer.py
    Made major changes to the code and shifted the GUI part to another file

--- article_summarizer.py
+++ article_summarizer.py
@@ -1,12 +1,12 @@
-import tkinter as tk
+from article_summarizer_gui import *
 from tkinter import messagebox
 import nltk
 from textblob import TextBlob
 from newspaper import Article
-nltk.download('punkt')
+
 def summarize():
 
-    url = utext.get('1.0',"end").strip()
+    url = urlText.get('1.0',"end").strip()
     if url == "":
         messagebox.showerror("Error", "No URL entered,\nplease enter a URL and then press the button")
         return
@@ -16,122 +16,62 @@ def summarize():
     article.parse()
 
     article.nlp()
-    
-    title.config(state='normal')
-    author.config(state='normal')
-    publication.config(state='normal')
-    summary.config(state='normal')
-    sentiment.config(state='normal')
 
-    title.delete('1.0','end')
-    title.insert('1.0',article.title)
+    titleField.config(state='normal')
+    authorField.config(state='normal')
+    publicationField.config(state='normal')
+    summaryField.config(state='normal')
+    sentimentField.config(state='normal')
 
-    author.delete('1.0','end')
-    author.insert('1.0',article.authors)
+    titleField.delete('1.0','end')
+    titleField.insert('1.0',article.title)
 
-    publication.delete('1.0','end')
-    publication.insert('1.0',str(article.publish_date))
+    authorField.delete('1.0','end')
+    authorField.insert('1.0',article.authors)
 
-    summary.delete('1.0','end')
-    summary.insert('1.0',article.summary)
+    publicationField.delete('1.0','end')
+    publicationField.insert('1.0',str(article.publish_date))
+
+    summaryField.delete('1.0','end')
+    summaryField.insert('1.0',article.summary)
+    summaryField['yscrollcommand'] = summaryScroll.set
 
     analysis=TextBlob(article.text)
-    sentiment.delete('1.0','end')
-    sentiment.insert('1.0',f'Polarity: {analysis.polarity}, Sentiment:{"POSITIVE" if analysis.polarity>0 else ("NEGATIVE" if analysis.polarity<0 else "NEUTRAL")}')
-    
-    title.config(state='disabled')
-    author.config(state='disabled')
-    publication.config(state='disabled')
-    summary.config(state='disabled')
-    sentiment.config(state='disabled')
+    sentimentField.delete('1.0','end')
+    sentimentField.insert('1.0',f'Polarity: {analysis.polarity}, Sentiment:{"POSITIVE" if analysis.polarity>0 else ("NEGATIVE" if analysis.polarity<0 else "NEUTRAL")}')
+
+    titleField.config(state='disabled')
+    authorField.config(state='disabled')
+    publicationField.config(state='disabled')
+    summaryField.config(state='disabled')
+    sentimentField.config(state='disabled')
+
 
 def clearBox ():
     
     #Function to clear all the fields when the clearButton is pressed
-    title.config(state='normal')
-    author.config(state='normal')
-    publication.config(state='normal')
-    summary.config(state='normal')
-    sentiment.config(state='normal')
+    titleField.config(state='normal')
+    authorField.config(state='normal')
+    publicationField.config(state='normal')
+    summaryField.config(state='normal')
+    sentimentField.config(state='normal')
     
-    utext.delete('1.0', 'end')
-    title.delete('1.0', 'end')
-    author.delete('1.0', 'end')
-    publication.delete('1.0', 'end')
-    summary.delete('1.0', 'end')
-    sentiment.delete('1.0', 'end')
+    urlText.delete('1.0', 'end')
+    titleField.delete('1.0', 'end')
+    authorField.delete('1.0', 'end')
+    publicationField.delete('1.0', 'end')
+    summaryField.delete('1.0', 'end')
+    sentimentField.delete('1.0', 'end')
 
-    title.config(state='disabled')
-    author.config(state='disabled')
-    publication.config(state='disabled')
-    summary.config(state='disabled')
-    sentiment.config(state='disabled')
+    titleField.config(state='disabled')
+    authorField.config(state='disabled')
+    publicationField.config(state='disabled')
+    summaryField.config(state='disabled')
+    sentimentField.config(state='disabled')
+
+summaryBtn.config(command=summarize)
+clearButton.config(command=clearBox)
 
 
-#Configuration of the Graphical User Interface
-    
-bgColor = "#352F44"
-secBgColor = "#5C5470"
-buttonColor = "#B9B4C7"
-textColor = "#FAF0E6"
-
-mainFont = "Helvetica"
-
-root = tk.Tk()
-root.title("News Summarizer")
-root.configure(bg=bgColor)
-
-heading = tk.Label(root, text = "ARTICLE SUMMARIZER", font=(mainFont, 20), bg=bgColor, fg=textColor)
-heading.grid(row = 0, column= 0, pady=4)
-
-inputFrame = tk.Frame(root, bg= secBgColor, padx=4, pady=4)
-inputFrame.grid(row = 1, column = 0)
-
-ulabel=tk.Label(inputFrame,text="URL", bg=secBgColor, fg = textColor, font = (mainFont, 10))
-ulabel.grid(row = 0, column = 0, padx=2)
-
-utext=tk.Text(inputFrame,height=2,bg=buttonColor, fg = bgColor, width = 55)
-utext.grid(row = 0, column = 1, padx=2, ipady=3)
-
-btn=tk.Button(inputFrame, text="Summarize", bg=buttonColor, fg = bgColor, font = (mainFont, 10), height=2, command=summarize)
-btn.grid(row = 0, column = 2, padx=2)
-
-tlabel = tk.Label(root, text="TITLE", font = (mainFont, 10), bg = bgColor, fg = textColor)
-tlabel.grid(row = 2, column = 0, sticky=tk.W, pady=(4, 0), padx=4)
-
-title = tk.Text(root, height=1)
-title.config(state='disabled', bg=secBgColor,fg=textColor, font = (mainFont, 10))
-title.grid(row = 3, column = 0, stick=tk.W, padx=6)
-
-aLabel = tk.Label(root, text="AUTHOR", font = (mainFont, 10), bg = bgColor, fg = textColor)
-aLabel.grid(row = 4, column = 0, sticky=tk.W, pady=(4, 0), padx=4)
-
-author = tk.Text(root, height=1)
-author.config(state='disabled', bg=secBgColor, fg=textColor, font = (mainFont, 10))
-author.grid(row = 5, column = 0, stick=tk.W, padx=6)
-
-pLabel = tk.Label(root, text="PUBLICATION DATE", font = (mainFont, 10), bg = bgColor, fg = textColor)
-pLabel.grid(row = 6, column = 0, sticky=tk.W, pady=(4, 0), padx=4)
-
-publication = tk.Text(root, height=1)
-publication.config(state='disabled', bg=secBgColor,fg=textColor, font = (mainFont, 10))
-publication.grid(row = 7, column = 0, stick=tk.W, padx=6)
-
-sLabel = tk.Label(root, text="SUMMARY", font = (mainFont, 10), bg = bgColor, fg = textColor)
-sLabel.grid(row = 8, column = 0, sticky=tk.W, pady=(4, 0), padx=4)
-
-summary = tk.Text(root)
-summary.config(state='disabled', bg=secBgColor,fg=textColor, font = (mainFont, 10), height=10)
-summary.grid(row = 9, column = 0, stick=tk.W, padx=6)
-
-saLabel = tk.Label(root, text="SENTIMENTAL ANALYSIS", font = (mainFont, 10), bg = bgColor, fg = textColor)
-saLabel.grid(row = 10, column = 0, sticky=tk.W, pady=(4, 0), padx=4)
-
-sentiment = tk.Text(root, height=1)
-sentiment.config(state='disabled', bg=secBgColor,fg=textColor, font = (mainFont, 10))
-sentiment.grid(row = 11, column = 0, stick=tk.W, padx=6, pady = (0, 8))
-
-clearBtn = tk.Button(root, text="CLEAR", bg=buttonColor, fg = bgColor, font = (mainFont, 10), height=1, command=clearBox, width = 70)
-clearBtn.grid(row = 12, column = 0, padx=2, pady= (0, 8))
 
 root.mainloop()
